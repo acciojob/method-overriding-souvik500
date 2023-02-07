b95710074bc0b22e8b4df62dfdfd19b749489de1
souvikadhikary <souvik500>
2023-02-08 02:24:44 +0530

    Over-ridden Program

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+    class A {
+        String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    class B extends A {
+        String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public void main(String[] args) {
+        B obj = new B();
+        obj.meth();
+    }
+
+
 }
