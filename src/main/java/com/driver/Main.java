a99684e71fdf30c1e0113eae2283e540bb16dcdb
souvikadhikary <souvik500>
2023-02-08 16:02:10 +0530

    Over-ridden Program

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,13 +1,13 @@
 package com.driver;
 
 public class Main {
-    static class A {
+    public static class A {
         String meth() {
             return "Method is overridden in Extendend class B";
         }
     }
 
-    static class B extends A {
+    public static class B extends A {
         @Override
         String meth() {
             return "Method is overridden in Extendend class B";
